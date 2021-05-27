1402a262a02c0261adf345b073902614718c75ad
chinonana <nanakachinone@gmail.com>
2021-05-27 14:25:20 +0900

    first commit

---
+++ date.php
@@ -0,0 +1,12 @@
+<?php
+$str='文字列です。str';
+$int_number=4;
+$array=['apple','orenge'];
+$array2=[
+	'apple'=>'りんご',
+	'orange'=>'オレンジ'];
+echo gettype($str).PHP_EOL;
+echo gettype($int_number).PHP_EOL;
+echo gettype($array).PHP_EOl;
+
+
---
+++ if.php
@@ -0,0 +1,12 @@
+<?php
+
+$input_number=$argv[1];
+if($input_number%2===0){
+	echo $input_number."は偶数です。".PHP_EDL;}
+else{
+	echo$input_number."は奇数です。".PHP_EDL;}
+
+
+
+?>  
+	
